bccfc30578230fc868ed4f1dfd8cce9f5fe44e6b
Johannes Döbler <jd@civilian-framework.org>
2018-09-14 09:43:02 +0200

    documentation

--- src/demo/org/jdbx/demo/BatchDemo.java
+++ src/demo/org/jdbx/demo/BatchDemo.java
@@ -17,8 +17,9 @@
 package org.jdbx.demo;
 
 
-import java.sql.Connection;
+import java.util.List;
 import org.jdbx.BatchResult;
+import org.jdbx.BatchResult.CountType;
 import org.jdbx.JdbxException;
 import org.jdbx.PrepStmt;
 import org.jdbx.StaticStmt;
@@ -33,37 +34,45 @@ import org.jdbx.StaticStmt;
 public class BatchDemo
 {
 	/**
-	 * Use {@link StaticStmt#batch()} to add batch cmds and run the batch.
+	 * Evaluate the result counts of a BatchResult.
 	 */
-	public void staticCmds(Connection con) throws JdbxException
+	public void evalResultCounts(StaticStmt stmt) throws JdbxException
 	{
-		try (StaticStmt stmt = new StaticStmt(con))
-		{
-			BatchResult<Void> result = stmt.batch()
-				.add("UPDATE StatusA SET flag = 0")
-				.add("UPDATE StatusB SET flag = 1")
-				.add("UPDATE StatusC SET flag = 2")
-				.run();
-			// ... evaluate result
-		}
+		// fill the batch, execute and obtain a BatchResult
+		BatchResult<Void> result = stmt.batch()
+			.add("UPDATE StatusA SET flag = 0")
+			.add("UPDATE StatusB SET flag = 1")
+			.add("UPDATE StatusC SET flag = 2")
+			.run();
+		
+		// evaluate result counts
+		int count0		= result.getCount(0); 		// get update count of first command
+		int[] counts 	= result.getCounts(); 		// all counts as array
+		CountType ct0	= result.getCountType(0);	// check if update succeeded, failed, etc.
+		
+		// assert result counts
+		// if update count of command 0 wasn't 15 then this will throw an exception
+		result.requireCount(0, 15);
+		// if update CountType of command 0 wasn't SUCCESS then this will throw an exception
+		result.requireCountType(0, CountType.SUCCESS);
 	}
 
 
 	/**
-	 * Use {@link PrepStmt#batch()} to add batch cmds and run the batch.
+	 * Evaluate the result value of a BatchResult.
+	 * @param 
 	 */
-	public void paramCmds(Connection con, int... ids) throws JdbxException
+	public void evalResultValue(PrepStmt pstmt) throws JdbxException
 	{
-		try (PrepStmt stmt = new PrepStmt(con))
-		{
-			stmt.init("UPDATE Status SET flag = 1 WHERE id = ?");
-			for (int id : ids)
-			{
-				stmt.param(1).setInt(id);
-				stmt.batch().add();
-			}
-			BatchResult<Void> result = stmt.batch().run();
-			// ... evaluate result
-		}
+		pstmt.init("INSERT INTO Names (name) values (?)");
+		pstmt.param(1, "Peter").batch().add();
+		pstmt.param(1, "Paul").batch().add();
+		
+		// batch should return the generated Integer ids
+		BatchResult<List<Integer>> result = pstmt.batch().runGetCols(Integer.class);
+		
+		// evaluate counts as before ...
+		// and get the ids 
+		List<Integer> ids = result.value();
 	}
 }
