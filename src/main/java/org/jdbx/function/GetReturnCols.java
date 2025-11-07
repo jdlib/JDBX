2354146386a58e23de5e9e80d937687fc730f549
Johannes Döbler <jd@civilian-framework.org>
2025-11-07 07:07:57 +0100

    javadoc

--- src/main/java/org/jdbx/function/GetReturnCols.java
+++ src/main/java/org/jdbx/function/GetReturnCols.java
@@ -7,17 +7,17 @@ import org.jdbx.QueryResult;
 
 /**
  * A functional interface for a service which reads the returned column values
- * from an update result.
+ * obtained from an update operation.
  * @see Statement#getGeneratedKeys()
  */
 @FunctionalInterface
 public interface GetReturnCols<V>
 {
 	/**
-	 * Reads the returned column values as a value of type V 
+	 * Reads the returned column values as a value of type V
 	 * @param updateCount the update count
 	 * @param result a QueryResult containing the returned column values
-	 * @return the value representing the returned column values 
+	 * @return the value representing the returned column values
 	 * @see Statement#getGeneratedKeys()
 	 * @throws Exception if an error occurs
 	 */
