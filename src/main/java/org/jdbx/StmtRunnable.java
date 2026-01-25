b60ff79ed2c3eb79737de6c68517865c7fb3fba9
Johannes Döbler <jd@civilian-framework.org>
2026-01-25 12:55:36 +0100

    javadoc

--- src/main/java/org/jdbx/StmtRunnable.java
+++ src/main/java/org/jdbx/StmtRunnable.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2016 JDBX
- * 
+ *
  * https://github.com/jdlib/JDBX
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
+ * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0.
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -34,8 +34,7 @@ abstract class StmtRunnable
 
 
 	/**
-	 * Returns a string describing this object.
-	 * @return a description
+	 * @return a string describing this object.
 	 */
 	@Override public final String toString()
 	{
